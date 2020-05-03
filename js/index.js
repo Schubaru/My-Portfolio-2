20c61a0419ceaee6c62317c0ae13c8c4d48aaad2
Schumungus <abschuzy@gmail.com>
2020-05-03 18:55:54 -0400

    Iterataion of Second portfolio
    jazzed it up with CSS. need to fix section headings and cards still. also would like to make the image move with scroll.

---
+++ js/index.js
@@ -0,0 +1,29 @@
+//$(".tabItem").on("click", function(){
+  //  $(this).removeClass("active").addClass("inactive");
+//});
+
+$(".tabItem").on("click", function(){
+    $(this).toggleClass("active");
+    if ($(this).hasClass("inactive")){
+        $(this).removeClass("inactive").addClass("active");
+    }
+    else {
+        $(this).addClass("inactive").removeClass("active");
+    }
+});
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
